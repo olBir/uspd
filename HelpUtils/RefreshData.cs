1f6157bea6d290afe538d2557e36b04da0d90db6
olBir <ol.birukov@gmail.com>
2014-03-16 20:39:27 +0200

    Main commit
    main

---
+++ HelpUtils/RefreshData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TableUtils;
+
+namespace HelpUtils
+{
+    public partial class RefreshData : Form
+    {
+        FillTable f = new FillTable();
+        public RefreshData()
+        {
+            InitializeComponent();
+        }
+
+        private void refreshDataButton_Click(object sender, EventArgs e)
+        {
+                f.changeValuesInDb();
+        }
+
+
+    }
+}
